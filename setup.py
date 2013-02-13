cacfac9a0942c2482df8b46479185a1e69c39458
Fantomas42 <fantomas42@gmail.com>
2013-02-13 14:53:58 +0100

    fix packaging script

--- setup.py
+++ setup.py
@@ -2,20 +2,26 @@
 from setuptools import setup
 from setuptools import find_packages
 
-import zinnia_threaded_comments
+__version__ = '0.1'
+__license__ = 'BSD License'
+
+__author__ = 'Fantomas42'
+__email__ = 'fantomas42@gmail.com'
+
+__url__ = 'https://github.com/Fantomas42/zinnia-threaded-comments'
 
 setup(
     name='zinnia-threaded-comments',
-    version=zinnia_threaded_comments.__version__,
+    version=__version__,
 
     description='Threaded comments for django-blog-zinnia',
     long_description=open('README.rst').read(),
 
     keywords='django, blog, weblog, zinnia, comments, threaded',
 
-    author=zinnia_threaded_comments.__author__,
-    author_email=zinnia_threaded_comments.__email__,
-    url=zinnia_threaded_comments.__url__,
+    author=__author__,
+    author_email=__email__,
+    url=__url__,
 
     packages=find_packages(exclude=['demo_zinnia_threaded_comments']),
     classifiers=[
@@ -28,7 +34,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Topic :: Software Development :: Libraries :: Python Modules'],
 
-    license=zinnia_threaded_comments.__license__,
+    license=__license__,
     include_package_data=True,
     zip_safe=False
 )
--- zinnia_threaded_comments/__init__.py
+++ zinnia_threaded_comments/__init__.py
@@ -1,12 +1,4 @@
 """zinnia_threaded_comments"""
-__version__ = '0.1'
-__license__ = 'BSD License'
-
-__author__ = 'Fantomas42'
-__email__ = 'fantomas42@gmail.com'
-
-__url__ = 'https://github.com/Fantomas42/zinnia-threaded-comments'
-
 from zinnia_threaded_comments.models import ThreadedComment
 from zinnia_threaded_comments.forms import ThreadedCommentForm
 
